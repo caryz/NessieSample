83ed341ed20871a868240b2487484df57b4f5186
caryz <caryz@terpmail.umd.edu>
2016-02-06 10:56:42 -0500

    added marker tracking

--- js/map.js
+++ js/map.js
@@ -1,10 +1,50 @@
 "use strict"
 
-// var map;
+var map
+var markers = []
 
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 8
-//   });
-// }
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 38.9339, lng: -77.1773},
+    zoom: 12
+  });
+}
+
+function drawAtmMarker(lat, lng, markerTitle, markerContent) {
+  var infowindow = new google.maps.InfoWindow({
+    content: markerContent
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat, lng},
+    map: map,
+    title: markerTitle
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+  markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
--- js/nessieAPI.js
+++ js/nessieAPI.js
@@ -2,21 +2,58 @@
 
 // --- Global Variables --- //
 var apiKey = "562af076e8f2edf99452364e33679082";
-getAtms()
-function getAtms() {
+var atms = null // atm array
+
+function main() {
+  // use default values (geolocation too annoying)
+  getAtms(38.8960952, -77.1333157)
+
+}
+
+function updateAtmLocation() {
+  var mapppp = map
+  var mapCenter = map.getCenter()
+  var lat = map.getCenter().lat()
+  var lng = map.getCenter().lng()
+  getAtms(lat, lng)
+}
+
+function getAtms(lat, lng) {
   $.ajax({
-    url: "http://api.reimaginebanking.com/atms?lat=38.9283&lng=-77.1753&rad=1&key=" + apiKey,
+    url: "http://api.reimaginebanking.com/atms?lat=" + lat + "&lng=" + lng + "&rad=10&key=" + apiKey,
     type: "GET",
     contentType: 'application/json',  // this is a required header
     success: function(resultsJson) {
-      console.log(resultsJson);
-      console.log(resultsJson.message);
-      document.write(resultsJson.data[0].address.street_name + "<br/>");
-      document.write(resultsJson.data[1].address.street_name + "<br/>");
+      atms = resultsJson
+      drawAllAtmMarkers()
     }
   });
 }
 
+function drawAllAtmMarkers() {
+  console.log(atms)
+  var atmData = atms.data
+  var atm = null
+  var i
+  for(i = 0; i < atmData.length; i++) {
+    atm = atmData[i]
+    var title = atm.name
+    var addr = atm.address
+    var content = "<h5>" + title +"</h5>" +
+                  "<b>Address: </b>" + addr.street_number + " " + addr.street_name + ", " +
+                  " " + addr.city + " " + addr.state + " " + addr.zip + "<br/>" +
+                  "<b>Hours: </b>" + atm.hours[0] + "<br/>" +
+                  "<b>Accessibility: </b>"
+     atm.accessibility ? content += "accessible as FUCK" : content += "get rekt m8"
+
+
+    drawAtmMarker(atm.geocode.lat, atm.geocode.lng, title, content)
+  }
+}
+
+function writeInHtml(documentId, content) {
+  document.getElementById(documentId).innerHTML = content
+}
 
 
 /*
